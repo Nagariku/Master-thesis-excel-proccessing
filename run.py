f2314592c65e18f21a53c904e1c0529db734c561
Nagariku <osipovgrisha.ru2@gmail.com>
2022-12-25 23:24:18 +0100

    First test with excels

---
+++ run.py
@@ -0,0 +1,7 @@
+from src import main
+
+def run():
+    main.main()
+
+if __name__ == "__main__":
+    run()
---
+++ src/main.py
@@ -0,0 +1,34 @@
+from src.methods import calculations,reporting,validation
+
+import pandas as pd
+import xlrd
+
+import os
+import glob
+from IPython.display import display
+
+
+
+def main():
+    path = os.getcwd()
+    path+= "\\Excel surveys"
+    csv_files = glob.glob(os.path.join(path, "*.xlsx"))
+    print(path)
+    print(csv_files)
+    
+    
+    for f in csv_files:
+    
+    # read the csv file
+        #df = pd.read_excel(f,usecols=[0,2])
+        df = pd.read_excel(f,index_col=None, na_values=['NA'], usecols="B")
+        print(df)
+        # print the location and filename
+        #print('Location:', f)
+        #print('File Name:', f.split("\\")[-1])
+        
+        # print the content
+        #print('Content:')
+        #display(df)
+        #print()
+    

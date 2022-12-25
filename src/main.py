e79ec8bfc3a431cea8f51ea2d38db87748eb5020
Dmytro Matsepa <92322153+dimanlm@users.noreply.github.com>
2022-12-26 00:03:19 +0100

    Fix directory paths for all platforms

--- src/main.py
+++ src/main.py
@@ -3,22 +3,21 @@ from src.methods import calculations,reporting,validation
 import pandas as pd
 import xlrd
 
-import os
+import os.path
 import glob
 from IPython.display import display
 
 
 
 def main():
-    path = os.getcwd()
-    path+= "\\Excel_surveys"
+    # Get the path of the current directory and add the folder name
+    path = os.path.join(os.getcwd(), "Excel_surveys")
+     # Get all the files in the folder
     csv_files = glob.glob(os.path.join(path, "*.xlsx"))
-    #print(path)
-    #print(csv_files)
-    
+    # print(path)
+    # print(csv_files)
     
     for f in csv_files:
-    
     # read the csv file
         #df = pd.read_excel(f,usecols=[0,2])
         df = pd.read_excel(f,index_col=None, na_values=['NA'], usecols="B")
